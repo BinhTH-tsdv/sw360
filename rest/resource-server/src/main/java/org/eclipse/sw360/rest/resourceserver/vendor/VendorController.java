adde5978a8cb1e34e05401ec75d1b073b202b56d
BinhTH-tsdv <binh1.tranhai@toshiba.co.jp>
2024-06-13 18:10:25 +0700

    feat(vendors): Add rest api for getting all vendors
    Signed-off-by: BinhTH-tsdv <binh1.tranhai@toshiba.co.jp>

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/vendor/VendorController.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/vendor/VendorController.java
@@ -1,3 +1,4 @@
+/* VendorController */
 /*
  * Copyright Siemens AG, 2017-2018. Part of the SW360 Portal Vendor.
  *
@@ -16,13 +17,16 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.apache.thrift.TException;
 import org.eclipse.sw360.datahandler.common.SW360Utils;
+import org.eclipse.sw360.datahandler.thrift.licenses.License;
 import org.eclipse.sw360.datahandler.thrift.vendors.Vendor;
 import org.eclipse.sw360.rest.resourceserver.core.HalResource;
 import org.eclipse.sw360.rest.resourceserver.core.RestControllerHelper;
+import org.eclipse.sw360.rest.resourceserver.user.UserController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.RepositoryLinksResource;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelProcessor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
@@ -44,6 +48,7 @@ import java.net.URI;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import javax.servlet.http.HttpServletResponse;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -62,102 +67,79 @@ public class VendorController implements RepresentationModelProcessor<Repository
     @NonNull
     private final RestControllerHelper restControllerHelper;
 
-    @Operation(
-            summary = "List all of the service's vendors.",
-            description = "List all of the service's vendors.",
-            tags = {"Vendor"}
-    )
-    @RequestMapping(value = VENDORS_URL, method = RequestMethod.GET)
-    public ResponseEntity<CollectionModel<EntityModel<Vendor>>> getVendors(
-            Pageable pageable,
-            HttpServletRequest request
-            ) throws TException, URISyntaxException, PaginationParameterException, ResourceClassNotFoundException {
+    @GetMapping(value = VENDORS_URL)
+//    public ResponseEntity<EntityModel<Vendor>> getVendors() {
+//        List<Vendor> vendors = vendorService.getVendors();
+//        return new ResponseEntity<>(vendors, HttpStatus.OK);
+//    }
+//    public List<Vendor> getVendors() {
+//        List<Vendor> vendors = vendorService.getVendors();
+//        List<EntityModel<Vendor>> vendorResources = new ArrayList<>();
+//        return vendorService.getVendors();
+//    }
+    public ResponseEntity<CollectionModel<EntityModel<Vendor>>> getVendors() throws TException {
         List<Vendor> vendors = vendorService.getVendors();
 
-        PaginationResult<Vendor> paginationResult = restControllerHelper.createPaginationResult(request, pageable, vendors, SW360Constants.TYPE_VENDOR);
         List<EntityModel<Vendor>> vendorResources = new ArrayList<>();
-        for (Vendor v: paginationResult.getResources()) {
-            Vendor embeddedVendor = restControllerHelper.convertToEmbeddedVendor(v);
-            vendorResources.add(EntityModel.of(embeddedVendor));
+        for (Vendor vendor : vendors) {
+            //HalResource<Vendor> halVendor = restControllerHelper.addEmbeddedVendor(vendor);
+            //HalResource halVendor = restControllerHelper.addEmbeddedVendor(vendor);
+            Vendor embeddedVendor = restControllerHelper.convertToEmbeddedVendor(vendor);
+            HalResource<Vendor> halVendor = new HalResource<>(embeddedVendor);
+
+            Vendor vendorByFullName = vendorService.getVendorByFullName(vendor.getFullname());
+            if(vendorByFullName != null) {
+                Link vendorSelfLink = linkTo(UserController.class)
+                        .slash("api" + VendorController.VENDORS_URL + "/" + vendorByFullName.getId()).withSelfRel();
+                halVendor.add(vendorSelfLink);
+            }
+
+//            Vendor embeddedVendor = restControllerHelper.convertToEmbeddedVendor(vendor);
+//            EntityModel<Vendor> vendorResource = EntityModel.of(embeddedVendor);
+//            vendorResources.add(vendorResource);
+
+//            Vendor vendorByFullName = Vendor vendor.fullname;
+
+//            if(vendor.getFullname() != null) {
+//                Link vendorSelfLink = linkTo(UserController.class)
+//                        .slash("api" + VendorController.VENDORS_URL + "/" + vendor.getId()).withSelfRel();
+//                halVendor.add(vendorSelfLink);
+//            }
+
+            vendorResources.add(halVendor);
         }
 
-        CollectionModel<EntityModel<Vendor>> resources;
-        if (vendors.size() == 0) {
-            resources = restControllerHelper.emptyPageResource(Vendor.class, paginationResult);
-        } else {
-            resources = restControllerHelper.generatePagesResource(paginationResult, vendorResources);
-        }
-
-        HttpStatus status = resources == null ? HttpStatus.NO_CONTENT : HttpStatus.OK;
-        return new ResponseEntity<>(resources, status);
+        CollectionModel<EntityModel<Vendor>> resources = CollectionModel.of(vendorResources);
+        return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 
-    @Operation(
-            summary = "Get a single vendor.",
-            description = "Get a single vendor by id.",
-            tags = {"Vendor"}
-    )
-    @RequestMapping(value = VENDORS_URL + "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<EntityModel<Vendor>> getVendor(
-            @Parameter(description = "The id of the vendor to get.")
-            @PathVariable("id") String id
-    ) {
-        Vendor sw360Vendor = vendorService.getVendorById(id);
-        HalResource<Vendor> halResource = createHalVendor(sw360Vendor);
-        return new ResponseEntity<>(halResource, HttpStatus.OK);
-    }
-
-    @Operation(
-            summary = "Create a new vendor.",
-            description = "Create a new vendor.",
-            tags = {"Vendor"}
-    )
-    @PreAuthorize("hasAuthority('WRITE')")
-    @RequestMapping(value = VENDORS_URL, method = RequestMethod.POST)
-    public ResponseEntity<?> createVendor(
-            @Parameter(description = "The vendor to be created.")
-            @RequestBody Vendor vendor
-    ) {
-        vendor = vendorService.createVendor(vendor);
-        HalResource<Vendor> halResource = createHalVendor(vendor);
-
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/{id}")
-                .buildAndExpand(vendor.getId()).toUri();
-
-        return ResponseEntity.created(location).body(halResource);
-    }
+//    public ResponseEntity<CollectionModel<EntityModel<Vendor>>> getVendors(
+//            Pageable pageable,
+//            HttpServletRequest request
+//            ) throws TException, URISyntaxException, PaginationParameterException, ResourceClassNotFoundException {
+//        List<Vendor> vendors = vendorService.getVendors();
+//
+//        PaginationResult<Vendor> paginationResult = restControllerHelper.createPaginationResult(request, pageable, vendors, SW360Constants.TYPE_VENDOR);
+//        List<EntityModel<Vendor>> vendorResources = new ArrayList<>();
+//        for (Vendor v: paginationResult.getResources()) {
+//            Vendor embeddedVendor = restControllerHelper.convertToEmbeddedVendor(v);
+//            vendorResources.add(EntityModel.of(embeddedVendor));
+//        }
+//
+//        CollectionModel<EntityModel<Vendor>> resources;
+//        if (vendors.size() == 0) {
+//            resources = restControllerHelper.emptyPageResource(Vendor.class, paginationResult);
+//        } else {
+//            resources = restControllerHelper.generatePagesResource(paginationResult, vendorResources);
+//        }
+//
+//        HttpStatus status = resources == null ? HttpStatus.NO_CONTENT : HttpStatus.OK;
+//        return new ResponseEntity<>(resources, status);
+//    }
 
     @Override
     public RepositoryLinksResource process(RepositoryLinksResource resource) {
         resource.add(linkTo(VendorController.class).slash("api" + VENDORS_URL).withRel("vendors"));
         return resource;
-    }
-
-    private HalResource<Vendor> createHalVendor(Vendor sw360Vendor) {
-        return new HalResource<>(sw360Vendor);
-    }
-
-    @Operation(
-            summary = "Export all vendors as Excel file.",
-            description = "Export all vendors as Excel file.",
-            tags = {"Vendor"}
-    )
-    @PreAuthorize("hasAuthority('WRITE')")
-    @GetMapping(value = VENDORS_URL + "/exportVendorDetails")
-    public ResponseEntity<?> exportVendor(HttpServletResponse response) throws TException {
-        try {
-            ByteBuffer buffer = vendorService.exportExcel();
-            String filename = String.format("vendors-%s.xlsx", SW360Utils.getCreatedOn());
-            response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", filename));
-            copyDataStreamToResponse(response, buffer);
-        } catch (Exception e) {
-            throw new TException(e.getMessage());
-        }
-        return ResponseEntity.ok(HttpStatus.OK);
-    }
-
-    private void copyDataStreamToResponse(HttpServletResponse response, ByteBuffer buffer) throws IOException {
-        FileCopyUtils.copy(buffer.array(), response.getOutputStream());
     }
 }
